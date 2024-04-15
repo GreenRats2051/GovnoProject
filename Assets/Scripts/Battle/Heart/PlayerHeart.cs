d5b3401e8b36e401fc8cf1aca3c6b6cab59f23e9
GreenRats2051 <143582652+GreenRats2051@users.noreply.github.com>
2024-04-15 12:40:36 +0300

    Two

--- Assets/Scripts/Battle/Heart/PlayerHeart.cs
+++ Assets/Scripts/Battle/Heart/PlayerHeart.cs
@@ -1,12 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+<<<<<<< Updated upstream
 using UnityEngine.SceneManagement;
+=======
+<<<<<<< HEAD
+using UnityEngine.SceneManagement;
+=======
+>>>>>>> 833d43396f565e5639f05fa1773f916b030055a7
+>>>>>>> Stashed changes
 using UnityEngine.UI;
 
 public class PlayerHeart : MonoBehaviour
 {
     int HeartPoints = 7;
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+>>>>>>> Stashed changes
     public List<Image> Hearts = new List<Image>();
     public string nameScene;
 
@@ -18,6 +29,13 @@ public class PlayerHeart : MonoBehaviour
         }
     }
 
+<<<<<<< Updated upstream
+=======
+=======
+    
+    public List<Image> Hearts = new List<Image>();
+>>>>>>> 833d43396f565e5639f05fa1773f916b030055a7
+>>>>>>> Stashed changes
     public void OnHeartLost()
     {
         Hearts[HeartPoints].fillAmount -= 0.5f;
